49731d761e77e2c3bdefcb837d91e6b83d72e7d1
nrobertson25 <nrobertson@ggc.edu>
2015-09-28 14:56:59 -0400

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,18 +1,16 @@
-function chkName()
+function chkPassword() 
 {
-    var myName = document.getElementById("custName");
-    
-    var pos = myName.value.search(/^[A-Z][a-z]+, ?[A-Z][a-z]+, ?[A-Z]\.?$/);
-    
-    if (pos != 0)
-    {
-        alert("The name you entered (" + myName.value + ") is not in the correct form. \n" +    "The correct form is: "+ "last-name, first-name, middle-initial \n" + "Please go back and fix your name");
-        myName.focus();
-        myName.select();
+    var init = document.getElementById("initial");
+    if (init.value == "") 
+	{
+        alert("You did not enter a password \n" + "Please enter one now");   
+        init.focus();
         return false;
     }
-    else
-    {
-        return true;   
+    else 
+	{
+        return true;
     }
+    
+    document.getElementById("myForm").onsubmit = chkPassword;
 }
