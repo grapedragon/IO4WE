1c3a74561a57669b4a8eb910a5d92799eaa023be
abryant6 <abryant6@ggc.edu>
2015-11-23 18:13:40 -0500

    logout page

---
+++ logout.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<?php
+session_start();
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Home Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel ="stylesheet">
+    <script src="js/respond.js"></script>
+</head>
+    <body background="img/backdrops/cbt.png">
+
+    <!--navbar-->
+    <?php
+      include_once 'navbar.php';
+    ?>
+
+    <div class="container">
+
+    <div class="row">
+            <div class = "col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <h1>You have successfully logged out</h1>
+            </div>
+    </div>
+
+    <!--jumbotron-->
+    <div class="header">
+      <img src="img/jumbotron-sm.png" class="img-responsive center-block"/>
+    </div>
+	
+	<br>
+	<!--login button-->
+    <div class = "row">
+        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
+          <a class="btn btn-default btn-block" href="login.html" role="button">Log In</a>
+        </div>
+		<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
+          <a class="btn btn-primary btn-block" href="register.html" role="button">Sign Up</a>
+        </div>
+        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+        </div>
+    </div>
+
+    <!-- javascript -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    </body>
+
+</html>
