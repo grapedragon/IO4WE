a02b89b5b4a666fbfaf21cc53101414dcbeee5dd
grapedragon <grapedragon@users.noreply.github.com>
2015-11-23 17:49:17 -0500

    Update regi.php

--- regi.php
+++ regi.php
@@ -1,78 +1,77 @@
-<head>
-    <meta charset="utf-8">
-    <title>Regi</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/respond.js"></script>
-</head>
-
 <?php
-
-$sent = false;
-
-if ( isset($_POST['register']) ) 
-    {
-        $error = array();
-
-        //Password validation: must contain at least 1 letter and number. Allows characters !@#$% and be 6-15 characters
-        if(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{6,15}$/', $_POST['password1']))
-        {
-            $error[]='The password does not match the requirements';
-        }
-        if($_POST['password1']!= $_POST['password2'])
-        {
-            $error[]='The passwords do not match';
-        }
-        //Email validation
-        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
-        {
-            $error[]='Invalid E-mail';
-        }       
-
-        if ( count($error) > 0) 
-        {
-            foreach ($error as $output) {
-               echo "{$output} <br>";
-            }
-        } else {
-            $sent = true;
-        }
-    }//end isset register
-
-?>
-
-<?php if ($sent==false) { ?>
-
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" name="register">
-        <div class="form-group">
-            <label for="password1">Password</label> 
-            <input type="password" maxlength="15" class="form-control" name="password1" placeholder="Password" />
-        </div>
-        <div class="form-group">
-            <label for="password2">Confirm Password</label>
-            <input type="password" maxlength="15" name="password2" class="form-control" placeholder="Confirm Password"/>
-        </div>
-        <div class="form-group">
-            <label for="email">Email</label> 
-            <input type="text" class="form-control" placeholder="your@email.com" maxlength="25"  name="email" value="<?php if (isset($_POST['email'])) {echo $_POST['email'];} ?>"/>
-        </div>
-        <button type="submit" class="btn btn-default" value="Register" name="register">Register</button>
-    </form>
-
-<?php } 
-else { 
-    include('connection.php');
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-$email = $_POST['email'];
-$password = $_POST['password2'];
-//if values are not empty, proceed to store them in the database
-if( !empty($email) && !empty($password))
-{
-    mysqli_query($dbc, "INSERT INTO user1(email, pw) VALUES ('$email', '$password')");
-		echo " row  inserted, everything worked fine!";	
-		}else{		
-			echo "ERROR: you left some values in blank!";	
+    error_reporting(0);
+    include("connection.php");
+    $login_email = $_POST['email'];
+    $login_password = $_POST['password'];
+    $query = mysqli_query($dbc, "SELECT * FROM users WHERE email='".$login_email."'");
+    $numrows = mysqli_num_rows($query);
+	if($_SERVER['REQUEST_METHOD'] == 'POST')
+		{
+			if($numrows != 0)
+			{
+				while($row = mysqli_fetch_array($query))
+				{
+					$dbemail = $row['email']; //must matching with the field name in your database table;
+					$dbpass = $row['pw'];
+					$dbfirstname = $row['first_name'];
+                    $dbisadmin = $row['isadmin'];
+         				
+				}	
+				if($login_email==$dbemail)
+				{
+					if($login_password==$dbpass)
+					{
+						//Successfull login -  check for admin
+                        session_start();
+                        if($dbisadmin === '1')
+                        {
+                            //admin redirect
+                            //alex you need to write where you want the admin to go
+                            echo " You are the Almighty Admin MUhahahahaha ";
+                            $_SESSION["isadmin"] = true;
+                            $_SESSION["email"] = $dbemail;
+                            // header('Location: admin.html');
+                            exit;
+                           // echo "admin " .$dbisadmin. " ." ; 
+                        }
+                        else
+                        {
+                            //student redirect
+                            echo " Oh a student fine";
+                            header('Location: gamePage.html');
+                             $_SESSION["isadmin"] = false;
+                            $_SESSION["email"] = $dbemail;
+                            exit;
+                           // echo "admin" .$dbisadmin. " . ";
+                        }
+                        
+                        
+					}
+		 			else 
+					{		
+						echo "your password is incorrect!";
+						//echo "hey there";
+                       // echo "USER ".$dbemail." ".$login_email." PASS ".$dbpass." ".$login_password.".";
+					}
+				}
+				else
+				{
+					echo "your email is incorrect!";
+					echo "email poop";
+                    echo "USER ".$dbemail." ".$login_email." PASS ".$dbpass." ".$login_password.".";
+				}	
+			}
+			else
+			{
+				echo "Invalid credentials! If you are not registered please register bellow...";
+                echo "hellow there";
+                echo "USER ".$dbemail." ".$login_email." PASS ".$dbpass." ".$login_password.".";
+			}
 		}
-	}else{
-	echo "<strong>Please complete the form...</strong>";
-} } ?>
+		else
+		{
+			echo "Please Login...";	
+			echo "last echo";
+            echo "USER ".$dbemail." ".$login_email." PASS ".$dbpass." ".$login_password.".";
+		}
+?>
