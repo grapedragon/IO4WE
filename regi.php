00db7a8633773ac07b05a1db6eb8a4559e17f4f6
grapedragon <grapedragon@users.noreply.github.com>
2015-11-30 15:34:56 -0500

    Adding second email and more

--- regi.php
+++ regi.php
@@ -19,7 +19,11 @@ if ( isset($_POST['register']) )
         if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
         {
             $error[]='Invalid E-mail';
-        }       
+        }     
+      if($_POST['email']!= $_POST['email2'])
+        {
+            $error[]='The Emails do not match';
+        }
 
         if ( count($error) > 0) 
         {
@@ -39,6 +43,11 @@ if ( isset($_POST['register']) )
        <label>Password1</label> <input type="password" maxlength="15" name="password1" /><br>
        <label>Password2</label> <input type="password" maxlength="15" name="password2" /><br>
        <label>Email</label> <input type="text" placeholder="your@email.com" maxlength="25"  name="email" value="<?php if (isset($_POST['email'])) {echo $_POST['email'];} ?>"/><br>
+       <label>Email</label> <input type="text" placeholder="your@email.com" maxlength="25"  name="email2" value="<?php if (isset($_POST['email2'])) {echo $_POST['email2'];} ?>"/><br>
+       <label>First Name</label> <input type="text" maxlength="25" name="fname" /><br>
+       <label>Last Name</label> <input type="text" maxlength="25" name="lname" /><br>
+       
+       
         
         <input type="submit" value="Register" name="register"/>
     </form>
@@ -49,12 +58,25 @@ else {
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 $email = $_POST['email'];
 $password = $_POST['password2'];
+$lname = $_POST['lname'];
+$fname = $_POST['fname'];
 //if values are not empty, proceed to store them in the database
 if( !empty($email) && !empty($password))
 {
     mysqli_query($dbc, "INSERT INTO users(email, pw) VALUES ('$email', '$password')");
 	//	echo " row  inserted, everything worked fine!";
-		include ('login.php')
+		if( !empty($lname))
+		{
+			mysqli_query($dbc, "INSERT INTO users() VALUES ('$lname')");
+			if( !empty($fname))
+			{
+				mysql
+			}
+		}
+		
+		
+		include ('login.php');
+		
 		}else{		
 			echo "ERROR: you left some values in blank!";	
 		}
