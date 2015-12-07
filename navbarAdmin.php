f5b3bc9b299105f3621cde164f0f0e5d305da277
nrobertson25 <nrobertson@ggc.edu>
2015-12-06 21:41:16 -0500

    admin navbar

---
+++ navbarAdmin.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  
+     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+              <!--li><a href ="index.php">Index</a></li>
+              <li><a href="login.php">Login</a></li-->
+			  
+              <li><a href="adminProfile.php">My Profile</a></li>
+              <li><a href="adminGamePage.php">Play Game</a></li>
+              <li><a href ="adminwhosonline.php">Whos Online</a></li>
+			  <li><a href ="admin.php">Administration</a></li>
+              <li><a href ="logout.php">Log Out</a></li>
+        </ul>
+      </div> <!-- /.collapse Navar-collapse -->
+     
+</nav>
