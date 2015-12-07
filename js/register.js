e2cfb57cc8922980f4d4f19e286c6b090d528e5c
nrobertson25 <nrobertson@ggc.edu>
2015-12-06 21:51:25 -0500

    Update register.js

--- js/register.js
+++ js/register.js
@@ -20,6 +20,9 @@ function chkName()
 function chkPassword() 
 {
     var init = document.getElementById("initial");
+	
+	var pos2 = init.value.search(/^[A-Z][a-z]+, ?[A-Z][a-z]+, ?[A-Z]\.?$/);
+	
     if (init.value == "") 
 	{
         alert("You did not enter a password \n" + "Please enter one now");   
@@ -30,6 +33,24 @@ function chkPassword()
     {
         return true;
     }
+}
+
+function chkPhone() 
+{
+  var myPhone = document.getElementById("telephone");
+  
+  var pos = myPhone.value.search(/^\d{3}-\d{3}-\d{4}$/);
+
+  if (pos != 0) 
+  {
+    alert("The phone number you entered (" + myPhone.value +
+          ") is not in the correct form. \n" +
+          "The correct form is: ddd-ddd-dddd \n" +
+          "Please go back and fix your phone number");
+    return false;
+  } 
+  else
+    return true;
 }
 
 
