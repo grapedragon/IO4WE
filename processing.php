cad0393bf763bbd5e4d22eb9f2ea63f3d858fcef
nrobertson25 <nrobertson@ggc.edu>
2015-12-06 21:43:31 -0500

    processing

--- processing.php
+++ processing.php
@@ -4,12 +4,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $email = $_POST['email'];
-//$phone = $_POST['phone'];
+$courseid = $_POST['course'];
+$phone = $_POST['phone'];
 $password = $_POST['password'];
 //if values are not empty, proceed to store them in the database
-if(!empty($fname) && !empty($lname) && !empty($email) /* && !empty($phone) */&& !empty($password)){
-		mysqli_query($dbc, "INSERT INTO users(first_name, last_name, email, pw) 
-		VALUES ('$fname', '$lname', '$email', '$password')");
+if(!empty($fname) && !empty($lname) && !empty($email) && !empty($phone) && !empty($password) && !empty($courseid)){
+		mysqli_query($dbc, "INSERT INTO users(first_name, last_name, email, phone, pw, course) 
+		VALUES ('$fname', '$lname', '$email', '$phone', '$password', '$courseid')");
 		echo " row  inserted, everything worked fine!";	
 		}else{		
 			echo "ERROR: you left some values in blank!";	
